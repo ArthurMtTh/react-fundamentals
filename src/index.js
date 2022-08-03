0eca96a09bda88c221d738801578788e56e8ecf9
Arthur <arthurmonteiro369@gmail.com>
2022-08-03 11:57:59 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//stylesheet archive import
+import './index.css'
+
+//components import
+import First from './components/basics/First';
+
+import ReacDOM from 'react-dom'
+import React from 'react'
+
+const tag = <strong>React</strong>
+
+ReacDOM.render(
+    <div>
+        Hello {tag}
+        <First></First>
+    </div>, 
+document.getElementById('root'))
