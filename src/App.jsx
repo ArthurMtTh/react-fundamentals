a1831fe9c433c4a00872657418b672f802c4adb8
Arthur <arthurmonteiro369@gmail.com>
2022-08-12 11:11:32 -0300

    Cards with prop children

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,28 @@ import React from "react";
 //components import
 import RandomNumber from './components/basics/First';
 import SecondParams from './components/basics/Second-params';
+import Card from "./components/layout/Card";
 
 const app = () => (
     <div>
-        <SecondParams
-            situation='student situation'
-            name='Dex'
-            grade={9.3} />
+        <Card title='First'>
+            <SecondParams
+                situation='student situation'
+                name='Dex'
+                grade={9.3} />
+        </Card>
+        
+        <Card title="Second">
+            <SecondParams
+                situation='student situation'
+                name='Jill'
+                grade={6.5} />
+        </Card>
 
-        <SecondParams
-            situation='student situation'
-            name='Jill'
-            grade={6.5} />
-
-        <RandomNumber min={1} max={99} />
+        <Card title='Radom Number'>
+            <RandomNumber min={1} max={99} />
+        </Card>
+        
     </div>
 );
 
