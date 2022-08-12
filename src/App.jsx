1664918b9fa7723bcbaffbd4b8b0499b3b677a49
Arthur <arthurmonteiro369@gmail.com>
2022-08-12 11:37:11 -0300

    flexbox with display-wrap study

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 
+//stylesheet
+import './App.css'
+
 //components import
 import RandomNumber from './components/basics/First';
 import SecondParams from './components/basics/Second-params';
 import Card from "./components/layout/Card";
 
 const app = () => (
-    <div>
-        <Card title='First'>
-            <SecondParams
-                situation='student situation'
-                name='Dex'
-                grade={9.3} />
-        </Card>
-        
-        <Card title="Second">
-            <SecondParams
-                situation='student situation'
-                name='Jill'
-                grade={6.5} />
-        </Card>
+    <div className="App">
+        <div className="Cards">
+            <Card title='First'>
+                <SecondParams
+                    situation='student situation'
+                    name='Dex'
+                    grade={9.3} />
+            </Card>
 
-        <Card title='Radom Number'>
-            <RandomNumber min={1} max={99} />
-        </Card>
-        
+            <Card title="Second">
+                <SecondParams
+                    situation='student situation'
+                    name='Jill'
+                    grade={6.5} />
+            </Card>
+
+            <Card title='Radom Number'>
+                <RandomNumber min={1} max={99} />
+            </Card>
+        </div>
     </div>
 );
 
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const card = (props) => {
     return (
         <div className="Card">
-            <div className="Title">{props.title}</div>
+            <div className="Title"><h3>{props.title}</h3></div>
             <div className="Content">{props.children}</div>
         </div>
     );
